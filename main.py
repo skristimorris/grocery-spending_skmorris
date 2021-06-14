47f2c92e5f4739e250d3cd25598ef756c6bba20a
Sarah M <skmorris@iastate.edu>
2021-06-13 22:11:40 -0400

    User input test print append to csv dataframe

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-#main
+# main
+#import flask
+from flask import Flask
+
+#define app in flask
+#main = Flask(__name__)
+
+# import csv data files
+import pandas as pd # access to pandas-defined objects is done via pd
+df = pd.read_csv("/Users/skm/Documents/MHCI/Summer 2021/HCI 584/Project/grocery-spending_skmorris/data/expenses.csv")
+
+#@app.route("/")
+#print(df) # show formated with leading index row
+
+#Enter Date of item
+Purchase_Date=input('Enter the date the item was purchased: ')
+Purchase_Price=input('Enter the price of the item: ')
+df2=df.append({'Purchase Date': Purchase_Date}, ignore_index=True)
+print(df2)
 
