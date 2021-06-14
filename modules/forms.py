7a800fa184350ce76d0705778402c1e9d8f7065a
Sarah M <skmorris@iastate.edu>
2021-06-14 14:36:00 -0400

    added in input fields for form

--- modules/forms.py
+++ modules/forms.py
@@ -1,5 +1,9 @@
-from wtforms import Form, SelectField
+from wtforms import Form, SelectField, StringField
 
 class AddItem(Form):
-    choices = [("Bakery", "Beverages", "Candy", "Dairy", "Deli", "Frozen", "Meat", "Produce", "Snacks")]
-    select = SelectField("Category:", choices=choices)
+    category = [("Bakery", "Beverages", "Candy", "Dairy", "Deli", "Frozen", "Meat", "Produce", "Snacks")]
+    name = StringField("Name")
+    category = SelectField("Category", choices=category)
+    price = StringField("Price")
+    quantity = StringField("Quantity")
+    date = StringField("Purchase Date")
