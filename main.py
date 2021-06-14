b1b92ab5c5c68c4918c636caf47058f48f787953
Sarah M <skmorris@iastate.edu>
2021-06-14 11:09:12 -0400

    added datetime import

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # main
+import datetime
+#Imports the date and time - primarily used for users to enter purchase dates in a date format
+
 #import flask
 from flask import Flask
 
@@ -15,6 +18,7 @@ df = pd.read_csv("/Users/skm/Documents/MHCI/Summer 2021/HCI 584/Project/grocery-
 #Enter Date of item
 Purchase_Date=input('Enter the date the item was purchased: ')
 Purchase_Price=input('Enter the price of the item: ')
-df2=df.append({'Purchase Date': Purchase_Date}, ignore_index=True)
+df2=df.append({'Purchase Date': Purchase_Date,'Purchase Price': Purchase_Price }, ignore_index=True)
+
 print(df2)
 
