e9269c96b91f4ed074fb015315677579d5e25bba
Sarah M <skmorris@iastate.edu>
2021-06-14 15:47:22 -0400

    create form.py w/ class add item

---
+++ forms.py
@@ -0,0 +1,11 @@
+# forms.py
+
+from wtforms import Form, SelectField, StringField
+
+class AddItem(Form):
+    category = [("Bakery", "Beverages", "Candy", "Dairy", "Deli", "Frozen", "Meat", "Produce", "Snacks")]
+    name = StringField("Name")
+    category = SelectField("Category", choices=category)
+    price = StringField("Price")
+    quantity = StringField("Quantity")
+    date = StringField("Purchase Date")
