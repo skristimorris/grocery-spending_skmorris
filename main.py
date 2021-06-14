71edf304bfa1e0888d5137fcb9f4a2cf52efcdd5
Sarah M <skmorris@iastate.edu>
2021-06-14 11:38:50 -0400

    tested flask set up

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 # main
-import datetime
-#Imports the date and time - primarily used for users to enter purchase dates in a date format
 
-#import flask
+# imports the date and time - used for users to enter purchase dates in a date format
+import datetime
+
+# import flask
 from flask import Flask
+# test flask
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    return "Welcome to Flask!"
 
 #define app in flask
 #main = Flask(__name__)
@@ -11,14 +18,12 @@ from flask import Flask
 # import csv data files
 import pandas as pd # access to pandas-defined objects is done via pd
 df = pd.read_csv("/Users/skm/Documents/MHCI/Summer 2021/HCI 584/Project/grocery-spending_skmorris/data/expenses.csv")
-
-#@app.route("/")
 #print(df) # show formated with leading index row
 
 #Enter Date of item
-Purchase_Date=input('Enter the date the item was purchased: ')
-Purchase_Price=input('Enter the price of the item: ')
-df2=df.append({'Purchase Date': Purchase_Date,'Purchase Price': Purchase_Price }, ignore_index=True)
+#Purchase_Date=input('Enter the date the item was purchased: ')
+#Purchase_Price=input('Enter the price of the item: ')
+#df2=df.append({'Purchase Date': Purchase_Date,'Purchase Price': Purchase_Price }, ignore_index=True)
 
-print(df2)
+#print(df2)
 
