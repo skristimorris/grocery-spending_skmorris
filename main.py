7485f4db591ad40c536928bf2d77c94c68fb5d9f
Sarah M <skmorris@iastate.edu>
2021-06-18 12:57:06 -0400

    create sqlite db

--- main.py
+++ main.py
@@ -2,14 +2,28 @@
 
 #import datetime # imports the date and time - used for users to enter purchase dates in a date format
 from flask import Flask, flash, render_template, request, redirect
+from pandas.io import sql
 from forms import AddItem
 import pandas as pd
 import sqlite3
 
 dbname = "data/expenses.csv" # import data from csv file
-df = pd.read_csv(dbname) # read csv and set to dataframe
+df = pd.read_csv(dbname) # read csv and set to dataframe df
 print(df)
 
+conn = sqlite3.connect("data.db") # connect to sqlite
+df.to_sql("expenses", conn, if_exists = "replace", index = False) # write records stored in df to sql db named expenses, drop table if it exists before inserting new values
+c = conn.cursor() # create cursor object
+c.execute("SELECT * FROM expenses") # execute query to select everything in expenses db
+for row in c.fetchall(): # return all results of query
+    print(row)
+conn.close() # close connection
+
+conn = sqlite3.connect("data.db") # connect to sqlite
+df_from_sql = pd.read_sql_query("SELECT * FROM expenses", con=conn) # read sql query into pandas dataframe
+print(df_from_sql)
+print(df_from_sql.dtypes)
+conn.close()
 
 
 app = Flask(__name__)
@@ -36,8 +50,8 @@ def additem():
 #    return render_template(data)
 
 
-if __name__ == "__main__":
-    app.run()
+#if __name__ == "__main__":
+#    app.run()
 
 
 
