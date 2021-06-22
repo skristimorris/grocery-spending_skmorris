6c4cd38978cf9f618e9f512172afb96d19a9d6fd
Sarah M <skmorris@iastate.edu>
2021-06-21 22:34:56 -0400

    added in comments, changed wording

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # main.py
 
-from flask import Flask, render_template, request, flash, redirect, Response
+from flask import Flask, render_template, request
 import pandas as pd
 import sqlite3 as sql
-from sqlite3 import Error
 import io
 import matplotlib.pyplot as plt
 import base64
-import csv
 
 df = pd.read_csv("data/expenses.csv") # read csv and set to dataframe df
 conn = sql.connect("data.db") # connect to sqlite and create database data.db
@@ -35,8 +33,6 @@ monthSum=sortedData.groupby('Month_Year')['price'].sum().to_frame().reset_index(
 #exit()
 
 #Data manipulation for MATPLOTLIB pie plot - preprocessing data columns
-
-
 categorySum=sortedData.groupby('category').sum()
 #print (categorySum)
 #slicePrice=categorySum['price']
@@ -50,6 +46,7 @@ categorySum=sortedData.groupby('category').sum()
 #plt.ylabel('')
 #plt.show()
 #exit()
+
 app = Flask(__name__)
 
 # FLASK:  INDEX 
@@ -69,10 +66,9 @@ def add_item():
                 request.form["category"],
                 request.form["price"],
                 request.form["quantity"],
-                request.form["date"])) # execute query to select everything in expenses db
-            df = pd.read_sql_query("SELECT * FROM expenses", con=conn)
-            df.to_csv("data/expenses.csv")
-            
+                request.form["date"])) 
+            df = pd.read_sql_query("SELECT * FROM expenses", con=conn) # execute query to select everything in expenses db
+            df.to_csv("data/expenses.csv") # write dataframe to expenses.csv file
             conn.commit() # apply changes
         except:
             print("Error")
@@ -98,18 +94,18 @@ def spending():
 #    return render_template(data)
 
 #MATPLOTLIB to show and plot certain statistics - first plot will be total spent in each category
-@app.route('/month_price_bar_plot.png', methods=["GET"])
+@app.route('/expenses_per_month', methods=["GET"])
 def catPlot():
     #Start data into memory buffer
     barImage=io.BytesIO()
     #Plot data from preprocessed data to get total expenses per month
     monthSum.plot.bar(x="Month_Year", y="price", color="orange", width=0.4, legend=None)
     #Label the x axis of the matplotlib plot
-    plt.xlabel("Month and Year")
+    plt.xlabel("Month")
     #Label the y axis of the matplotlib plot
-    plt.ylabel("Total expenses in all categories")
+    plt.ylabel("Total Expenses")
     #Label the title of the matplotlib plot
-    plt.title("Total price of all expenses per month")
+    plt.title("Total Expenses per Month")
     #Bring in x-label by using tight layout function from matplotlib
     plt.tight_layout()
     #Save the figure matplotlib bar plot
@@ -120,9 +116,9 @@ def catPlot():
     #Encode and decode image byte data
     linkedImage=base64.b64encode(barImage.getvalue()).decode('utf8')
     #Process html template where image source of mont_price_bar_plot is stored
-    return render_template('cat_plot.html', plot_url=linkedImage)
+    return render_template('expenses_month.html', plot_url=linkedImage)
 
-@app.route('/cat_pie_plot.png', methods=["GET"])
+@app.route('/expenses_per_category', methods=["GET"])
 def piePlot():
     #Start data into memory buffer
     pieImage=io.BytesIO()
@@ -142,7 +138,7 @@ def piePlot():
     #Encode and decode image byte data
     linkedpieImage=base64.b64encode(pieImage.getvalue()).decode('utf8')
     #Process html template where image source of mont_price_bar_plot is stored
-    return render_template('pie_plot.html', plot_url=linkedpieImage)
+    return render_template('expenses_category.html', plot_url=linkedpieImage)
 
 
 if __name__ == "__main__":
