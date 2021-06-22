0787ed8fb2dbb9f14ba9cf54f05e9bc575c757a4
Sarah M <skmorris@iastate.edu>
2021-06-21 22:33:52 -0400

    added code in here for future

---
+++ tmp/tmp.py
@@ -0,0 +1,8 @@
+db = "/Users/skm/Documents/MHCI/Summer 2021/HCI 584/Project/grocery-spending_skmorris/data/expenses.csv"
+df = pd.read_csv(db)
+
+
+    #form = AddItem(request.form)
+    #Save item data on submit button press
+ #   if request.method == 'POST' and form.validate():
+ #           save_changes()
