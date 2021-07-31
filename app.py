6e730aa029229c7cdb44f53c2d11c4294c512a8d
Sarah M <skmorris@iastate.edu>
2021-07-31 16:14:18 -0400

    fixed callbacks for add item to update graphs

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from dash_core_components.Graph import Graph
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
+from pandas.core.indexes import interval
 from pandas.io.formats import style
 import plotly.express as px
 import pandas as pd
@@ -28,12 +29,6 @@ df['month_year'] = pd.to_datetime(df['date']).dt.strftime('%B %Y')
 df = df.sort_values(by='date').reset_index(drop=True)
 print(df)
 
-'''
-# group df by month_year and category - am i using this?
-df_group = df.groupby(['month_year', 'category']).sum().sort_values(by=['month_year'], ascending=True)
-print(df_group)
-'''
-
 # df for table
 df_table = df[['name', 'price', 'quantity', 'date']]
 print(df_table)
@@ -41,7 +36,6 @@ print(df_table)
 # create variable for current date in format of month & year to assign as default value for date dropdown
 today = date.today()
 current_MY = today.strftime('%B %Y')
-
 
 # Ref: https://dash.plotly.com/layout
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
@@ -60,7 +54,6 @@ def InputItem():
                 placeholder='Enter grocery item',
                 style={'width': '100%'}
             ),
-            html.Div(id='output-name'),
             html.Br(),
             html.P('Category', style={
                 'textAlign': 'left'
@@ -71,7 +64,6 @@ def InputItem():
                     {'label': i, 'value': i} for i in sorted(df_category.Category)
                 ],
             ),
-            html.Div(id='output-category'),
             html.Br(),
             html.P('Price', style={
                 'textAlign': 'left'
@@ -82,7 +74,6 @@ def InputItem():
                 placeholder='Enter price of item',
                 style={'width': '100%'}
             ),
-            html.Div(id='output-price'),
             html.Br(),
             html.P('Quantity', style={
                 'textAlign': 'left'
@@ -101,7 +92,6 @@ def InputItem():
                 },
                 value=1,
             ),
-            html.Div(id='output-quantity'),
             html.Br(),
             html.P('Date', style={
                 'textAlign': 'left'
@@ -110,7 +100,6 @@ def InputItem():
                 id='date',
                 month_format='MMM Do, YY'
             ),
-            html.Div(id='output-date'),
             html.Br(),
             html.Br(),
             html.Div(id='output-add-item')
@@ -221,7 +210,7 @@ dashboard = html.Div(
                 ),
         ],
         style={
-            'margin-left': '25%',
+            'margin-left': '15%',
             'margin-right': '5%',
             'padding': '20px 10px'
         }
@@ -260,11 +249,22 @@ sidebar = html.Div(
         'top': 0,
         'left': 0,
         'bottom': 0,
-        'width': '20%',
+        'width': '10%',
         'padding': '20px 10px',
         'background-color': '#f8f9fa'
     }
 )
+
+# callback for modal
+@app.callback(
+    Output('modal', 'is_open'),
+    [Input('button-new-item', 'n_clicks'), Input('close', 'n_clicks')],
+    [State('modal', 'is_open')],
+)
+def toggle_modal(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
 
 # Ref: https://dash.plotly.com/basic-callbacks
 # callback to set category dropdown options based on month selected - not selecting cat for month
@@ -284,19 +284,21 @@ def set_cat_option(month_year):
 def set_cat_default(available_options):
     return available_options[0]['value']
 
+
 # Ref: https://plotly.com/python/pie-charts/
 # callback to display graph for all categories
 @app.callback(
     Output('graph-spending-all', 'figure'),
-    [Input('dash-monthyear', 'value')]
+    [Input('table-item', 'data'),
+    Input('dash-monthyear', 'value')]
 )
-def update_graph_spending_all(month_year):
-    fig = px.pie(df.query('month_year == @month_year'), 
+def generate_graph_all_cat(data, month_year):
+    dff = pd.DataFrame.from_dict(data)
+    fig = px.pie(dff.query('month_year == @month_year'), 
         values='total', 
         names='category', 
         title= 'Spending for All Categories in {}'.format(month_year),
-        hole= .5,
-    )
+        hole= .5)
     fig.update_traces(
         hoverinfo='label+percent', 
         #text=['$' + total for total in fig.total.values],
@@ -305,20 +307,22 @@ def update_graph_spending_all(month_year):
     fig.update_layout(
         annotations= [
             dict(text= 'Total Amount <br> $', x=0.5, y=0.5, font_size=15, showarrow=False),
-            ],
+        ],
             legend_title='<b> Category </b>'
     )
-    return fig
+    return fig  
 
 # Ref: https://plotly.com/python/pie-charts/
 # callback to display graph for selected category
 @app.callback(
     Output('graph-item', 'figure'),
-    [Input('dash-monthyear', 'value'),
+    [Input('table-item', 'data'),
+    Input('dash-monthyear', 'value'),
     Input('dash-category', 'value')]
 )
-def update_graph_item(month_year, category):
-    fig = px.pie(df.query('month_year == @month_year and category ==@category'), 
+def update_graph_item(data, month_year, category):
+    dff = pd.DataFrame.from_dict(data)
+    fig = px.pie(dff.query('month_year == @month_year and category ==@category'), 
         values='total', 
         names='name', 
         title= 'Spending for {} in {}'.format(category, month_year),
@@ -341,10 +345,12 @@ def update_graph_item(month_year, category):
 # callback to display graph for selected category & month
 @app.callback(
     Output('graph-trend', 'figure'),
-    [Input('dash-category', 'value')]
+    [Input('table-item', 'data'),
+    Input('dash-category', 'value')]
 )
-def update_graph_trend(category):
-    fig = px.bar(df.query('category == @category'), x='month_year', y='price', color='category', barmode='group', 
+def update_graph_trend(data, category):
+    dff = pd.DataFrame.from_dict(data) 
+    fig = px.bar(dff.query('category == @category'), x='month_year', y='price', color='category', barmode='group', 
         title= 'Spending History for {}'.format(category),
         labels={
             'category': 'Category', 'price': 'Total Amount', 'month_year': 'Month of Purchase'
@@ -352,23 +358,13 @@ def update_graph_trend(category):
     )
     return fig
 
-# callback for modal
-@app.callback(
-    Output('modal', 'is_open'),
-    [Input('button-new-item', 'n_clicks'), Input('close', 'n_clicks')],
-    [State('modal', 'is_open')],
-)
-def toggle_modal(n1, n2, is_open):
-    if n1 or n2:
-        return not is_open
-    return is_open
+    # THIS WORKS except it's adding blank values to df - need exception - look at python exercise #2 for ideas with email exceptions 
 
-
-# THIS WORKS except it's adding blank values to df - need exception - look at python exercise #2 for ideas with email exceptions -- not working bc can't have id for 2 outputs
+# THIS WORKS except it's adding blank values to df - need exception - look at python exercise #2 for ideas with email exceptions 
 # Ref: https://dash.plotly.com/advanced-callbacks
-# callback to add new item to df + item.csv and update table with new row on submit
+# callback to add new item to df + item.csv
 @app.callback(
-    Output('output-add-item', 'children'),
+    Output('table-item', 'data'),
     [Input('submit-new-item', 'n_clicks')],
     [State('name', 'value'),
     State('category', 'value'),
@@ -385,49 +381,15 @@ def add_item(n, name, category, price, quantity, date):
         new_row = {'name': name, 'category': category, 'price': price, 'quantity': quantity, 'date': date}
         df = df.append(new_row, ignore_index=True)
         df.to_csv("data/items.csv", index=False)
+        df['total'] = df['price'] * df['quantity']
+        df['month_year'] = pd.to_datetime(df['date']).dt.strftime('%B %Y')
+        df = df.sort_values(by='date').reset_index(drop=True)
         print(df)
-        return '{} Added.'.format(name)
+        return df.to_dict('records')
     else:
         return dash.no_update
 
 '''
-# callback to add new item to df + item.csv and update table with new row on submit
-@app.callback(
-    Output('table-container', 'children'),
-    [Input('submit-new-item', 'n_clicks'),
-    Input('dash-category', 'value'),
-    Input('dash-monthyear', 'value')],
-    [State('name', 'value'),
-    State('category', 'value'),
-    State('price', 'value'),
-    State('quantity', 'value'),
-    State('date', 'date')
-    ]
-)
-def update_table(n, dash_cat, month_year, name, category, price, quantity, date):
-    ctx = dash.callback_context
-    input_id = ctx.triggered[0]['prop_id'].split('.')[0]
-    if input_id == 'dash-category' or 'dash-monthyear':
-        df = pd.read_csv("data/items.csv")
-        df_table = pd.DataFrame(df.query('month_year == @month_year and category == @dash_cat'))
-        df_table = df_table[['name', 'price', 'quantity', 'total', 'date']]
-        print(df_table)
-        return df.to_dict('records')
-    #elif input_id == 'submit-new-item':
-    #    df = pd.read_csv("data/items.csv")
-    #    new_row = {'name': name, 'category': category, 'price': price, 'quantity': quantity, 'date': date}
-    #    df = df.append(new_row, ignore_index=True)
-    #    df.to_csv("data/items.csv", index=False)
-    #    print(df)
-    #    df_table = pd.DataFrame(df.query('month_year == @month_year and category == @dash_cat'))
-    #    df_table = df_table[['name', 'price', 'quantity', 'total', 'date']]
-    #    print(df_table)
-    #    return df.to_dict('records')
-    #else:
-    #return dash.no_update
-'''
-
-
 # callback to update table based on category and date
 @app.callback(
     Output('table-item', 'data'),
@@ -439,7 +401,7 @@ def update_table(month_year, category):
     df_table = df_table[['name', 'price', 'quantity', 'total', 'date']]
     print(df_table)
     return df_table.to_dict('records')
-
+    '''
         
 app.layout = html.Div([navbar, sidebar, dashboard])
 
