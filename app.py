2382dd8e95496b693c6aa651749c2686c607ff51
Sarah M <skmorris@iastate.edu>
2021-08-01 13:23:51 -0400

    close modal on submit new item

--- app.py
+++ app.py
@@ -275,11 +275,12 @@ sidebar = html.Div(
 @app.callback(
     Output('modal', 'is_open'),
     [Input('button-new-item', 'n_clicks'), 
-    Input('close', 'n_clicks')],
+    Input('close', 'n_clicks'),
+    Input('submit-new-item', 'n_clicks')],
     [State('modal', 'is_open')],
 )
-def toggle_modal(n1, n2, is_open):
-    if n1 or n2:
+def toggle_modal(n1, n2, n3, is_open):
+    if n1 or n2 or n3:
         return not is_open
     return is_open
 
@@ -288,7 +289,6 @@ def toggle_modal(n1, n2, is_open):
     Output('category', 'value'),
     Output('price', 'value'),
     Output('quantity', 'value')],
-    #Output('date', 'date')],
     [Input('modal', 'is_open')]
 )
 def clear_input(n):
